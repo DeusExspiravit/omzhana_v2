df4acc37f1e30dbbdce877529eaf71a8913e9a88
Arvin Prince <85603191+DeusExspiravit@users.noreply.github.com>
2026-03-15 10:26:51 +0530

    created the home page with styling and have made the nav menu more better

--- website/store/urls.py
+++ website/store/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path 
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
     path('base/', views.base, name='base'),
+    path('cart/', views.cart, name='cart'),
+    path('shop/', views.shop, name='shop'),
 ]
--- website/store/views.py
+++ website/store/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 
+
 def base(request):
     return render(request, 'store/base.html')
 
+
 def home(request):
     return render(request, 'store/home.html')
+
+
+def about(request):
+    return render(request, 'store/about.html')
+
+
+def cart(request):
+    return render(request, 'store/cart.html')
+
+
+def shop(request):
+    return render(request, 'store/shop.html')
